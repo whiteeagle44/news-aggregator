f54fd0ffbd85ec840a17a9ef50b614e6add58a7b
whiteeagle44 <commandodev@protonmail.com>
2023-10-23 16:08:06 +0200

    fetch for recent news periodically

--- newsproducer/src/main/java/io/eagle44/newsproducer/NewsProducerApplication.java
+++ newsproducer/src/main/java/io/eagle44/newsproducer/NewsProducerApplication.java
@@ -1,14 +1,10 @@
 package io.eagle44.newsproducer;
 
-import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class NewsProducerApplication {
 
--- newsproducer/src/main/java/io/eagle44/newsproducer/news/NewsController.java
+++ newsproducer/src/main/java/io/eagle44/newsproducer/news/NewsController.java
@@ -1,5 +1,6 @@
 package io.eagle44.newsproducer.news;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +8,16 @@ import java.util.List;
 
 @RestController
 class NewsController {
+    private final static long ONE_HOUR_IN_MILLISECONDS = 3_600_000;
+    private final static long NEWS_REFRESH_TIME_IN_MILLISECONDS = 2 * ONE_HOUR_IN_MILLISECONDS;
+
     private final NewsService newsService;
 
     public NewsController(NewsService newsService) {
         this.newsService = newsService;
     }
 
+    @Scheduled(fixedRate = NEWS_REFRESH_TIME_IN_MILLISECONDS)
     @GetMapping("/load-recent-news")
     void getNews() {
         newsService.getNews("");
