2bc929e77c78ae24fcfec1d4103f45bb0a6d33bd
whiteeagle44 <commandodev@protonmail.com>
2023-10-23 15:19:30 +0200

    make NewsDTO public

--- newsconsumer/src/main/java/io/eagle44/newsconsumer/news/NewsDTO.java
+++ newsconsumer/src/main/java/io/eagle44/newsconsumer/news/NewsDTO.java
@@ -2,5 +2,5 @@ package io.eagle44.newsconsumer.news;
 
 import java.time.LocalDateTime;
 
-record NewsDTO(String title, String url, String description, LocalDateTime publicationDate, String source, String imageURL) {
+public record NewsDTO(String title, String url, String description, LocalDateTime publicationDate, String source, String imageURL) {
 }
